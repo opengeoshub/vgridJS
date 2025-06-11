3b7443ebdd9eda08e59e4cfcf3e44dac43c167d0
thangqd <quachdongthang@gmail.com>
2025-06-11 17:24:59 +0700

    initialize project

---
+++ website/.docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("D:\\Github\\vgriddggs\\website\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
+  require("D:\\Github\\vgriddggs\\website\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("D:\\Github\\vgriddggs\\website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("D:\\Github\\vgriddggs\\website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("D:\\Github\\vgriddggs\\website\\src\\css\\custom.css"),
+];
